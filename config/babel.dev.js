28c19e45e959cecec57009645efe5997f1347525
sebastian.bogado <sebastian.j.bogado@gmail.com>
2016-11-18 03:54:32 -0300

    Added hot module replacement so that there's no need to reload the whole page when updating a component

--- config/babel.dev.js
+++ config/babel.dev.js
@@ -3,11 +3,13 @@ module.exports = {
   presets: [
     'babel-preset-es2015',
     'babel-preset-es2016',
-    'babel-preset-react'
+    'babel-preset-react',
+    'babel-preset-react-hmre'
   ].map(require.resolve),
   plugins: [
     'babel-plugin-syntax-trailing-function-commas',
     'babel-plugin-transform-class-properties',
-    'babel-plugin-transform-object-rest-spread'
+    'babel-plugin-transform-object-rest-spread',
+    'react-hot-loader/babel'
   ].map(require.resolve)
 };
