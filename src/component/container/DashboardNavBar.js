b91f5d8a6a2117d680b601d17721388ff8e59fea
sebastian.bogado <sebastian.j.bogado@gmail.com>
2016-12-11 21:18:59 -0300

    identation fixes lol

--- src/component/container/DashboardNavBar.js
+++ src/component/container/DashboardNavBar.js
@@ -22,38 +22,38 @@ export default class DashboardNavBar extends Component {
 
   render() {
     return (
-        <Navbar fixedTop={true} bsStyle="inverse" role="navigation" fluid>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">Near.B</a>
-            </Navbar.Brand>
-            <Navbar.Toggle/>
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullRight={true}>
-              <NavDropdown eventKey={3} title={<Fa name="bell" fixedWidth={true}/>} id="alert-dropdown">
-                <MenuItem eventKey={1} href="#">Alerts</MenuItem>
-              </NavDropdown>
-              <NavDropdown title={<Fa name="user" fixedWidth={true}/>} id="user-dropdown">
-                <MenuItem eventKey={1} href="#">{this.state.userId}</MenuItem>
-              </NavDropdown>
-            </Nav>
-              <Nav bsClass="nav navbar-nav side-nav">
-                <NavItem eventKey={1} href={`/users/${this.state.userId}/`}>
-                  <Fa name="home" fixedWidth={true}/> Dashboard
-                </NavItem>
-                <NavItem eventKey={2} href={`/users/${this.state.userId}/stores`}>
-                  <Fa name="map-marker" fixedWidth={true}/> Stores
-                </NavItem>
-                <NavItem eventKey={3} href={`/users/${this.state.userId}/products`}>
-                  <Fa name="truck" fixedWidth={true}/> Warehouse
-                </NavItem>
-                <NavItem eventKey={4} href={`/users/${this.state.userId}/marketing`}>
-                  <Fa name="users" fixedWidth={true}/> Marketing
-                </NavItem>
-              </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+      <Navbar fixedTop={true} bsStyle="inverse" role="navigation" fluid>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Near.B</a>
+          </Navbar.Brand>
+          <Navbar.Toggle/>
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight={true}>
+            <NavDropdown eventKey={3} title={<Fa name="bell" fixedWidth={true}/>} id="alert-dropdown">
+              <MenuItem eventKey={1} href="#">Alerts</MenuItem>
+            </NavDropdown>
+            <NavDropdown title={<Fa name="user" fixedWidth={true}/>} id="user-dropdown">
+              <MenuItem eventKey={1} href="#">{this.state.userId}</MenuItem>
+            </NavDropdown>
+          </Nav>
+          <Nav bsClass="nav navbar-nav side-nav">
+            <NavItem eventKey={1} href={`/users/${this.state.userId}/dashboard`}>
+              <Fa name="home" fixedWidth={true}/> Dashboard
+            </NavItem>
+            <NavItem eventKey={2} href={`/users/${this.state.userId}/stores`}>
+              <Fa name="map-marker" fixedWidth={true}/> Stores
+            </NavItem>
+            <NavItem eventKey={3} href={`/users/${this.state.userId}/products`}>
+              <Fa name="truck" fixedWidth={true}/> Warehouse
+            </NavItem>
+            <NavItem eventKey={4} href={`/users/${this.state.userId}/marketing`}>
+              <Fa name="users" fixedWidth={true}/> Marketing
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
