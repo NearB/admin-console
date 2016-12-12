5cd6189bfca83229279b508e69099c84d8fccaa0
sebastian.bogado <sebastian.j.bogado@gmail.com>
2016-12-11 22:57:18 -0300

    Fixed navigation to single store lol

--- src/component/container/stores/Store.js
+++ src/component/container/stores/Store.js
@@ -9,12 +9,6 @@ import {
     Grid, Row, Col
 } from 'react-bootstrap';
 
-const styles = {
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-};
 
 export default class Store extends Component {
   constructor(props) {
@@ -24,34 +18,27 @@ export default class Store extends Component {
       userId: props.userId != null ? props.userId  : props.params.userId,
       storeId: props.storeId != null ? props.storeId  : props.params.storeId
     };
-
-    this.renderContent = this.renderContent.bind(this);
-  }
-
-  renderContent() {
-    return (
-        <div id="page-wrapper" style={styles.root}>
-          <Grid>
-            <Row>
-              <Col lg={6}>
-                <Orders storeId={this.state.storeId}></Orders>
-              </Col>
-              <Col lg={6}>
-                <Carts storeId={this.state.storeId}></Carts>
-              </Col>
-            </Row>
-            <Row>
-              <ContentManager storeId={this.state.storeId}/>
-            </Row>
-          </Grid>
-        </div>
-    );
   }
 
   render(){
     return (
-        <NavedContainer userId={this.state.userId}
-                        content={this.renderContent}/>
+      <NavedContainer userId={this.state.userId}>
+        <Grid fluid>
+          <Row>
+            <Col lg={6}>
+              <Orders storeId={this.state.storeId}></Orders>
+            </Col>
+            <Col lg={6}>
+              <Carts storeId={this.state.storeId}></Carts>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={12}>
+              <ContentManager storeId={this.state.storeId}/>
+            </Col>
+          </Row>
+        </Grid>
+      </NavedContainer>
     );
   }
 }
